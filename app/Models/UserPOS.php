b7b01970cb18a583a3b9e2ec1a343c1b024344f0
sileysa <sileysafaedatulnuraini984@gmail.com>
2026-04-20 22:19:18 +0700

    Menambahkan model dan relasi database

--- app/Models/UserPOS.php
+++ app/Models/UserPOS.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Users extends Model
+class UserPOS extends Model
 {
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
